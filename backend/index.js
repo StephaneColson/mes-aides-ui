46bcf5f4b1d4455459dab777d4fa7f3a9fddfbf2
Alexandre Segura <mex.zktk@gmail.com>
2018-07-20 16:02:03 +0200

    Enable Sentry in backend.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/config');
+var Raven = require('raven');
 
 // Setup mongoose
 require('./config/mongoose')(mongoose, config);
@@ -8,7 +9,18 @@ require('./config/mongoose')(mongoose, config);
 // Setup Express
 var app = express();
 
+// Enable Sentry in production
+// It expects SENTRY_DSN environment variable to be defined
+if (app.get('env') === 'production') {
+    Raven.config().install();
+    app.use(Raven.requestHandler());
+}
+
 app.use(require('./lib/ludwig')(mongoose, mongoose.model('Situation')));
 app.use(require('./config/api'));
+
+if (app.get('env') === 'production') {
+    app.use(Raven.errorHandler());
+}
 
 module.exports = app;
